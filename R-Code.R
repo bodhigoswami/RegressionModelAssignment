af0553fe15c86ba9d0bf1d483d211f3bc8edb938
Bodhi Goswami <90079160+bodhigoswami@users.noreply.github.com>
2021-10-27 19:07:25 -0700

    Dommit #2

--- R-Code.R
+++ R-Code.R
@@ -5,15 +5,15 @@ mtcars$am<-as.factor(mtcars$am)
 
 #Plot Showing Higher MPG for Manual Cars over Automatic Cars
 g = ggplot(mtcars, aes( x = as.factor(am), y = mpg))
-g = g + geom_boxplot(aes(fill=as.factor(am)), aplha = 0.35)
+g = g + geom_boxplot(aes(fill=as.factor(am), alpha = 0.35))
 g = g + geom_point(size =4, col = "purple", alpha = 0.75)
-g = g + xlab("Transmission Type (0 for Automatic, 1 for Manual") +
+g = g + xlab("Transmission Type (0 for Automatic, 1 for Manual)") +
             ylab("Miles per Gallon (MPG)") + ggtitle("Mpg v/s Transmission")
 g = g + theme(plot.title = element_text(hjust='0.5'))
 g = g + labs(fill = "Transmission")
 g
 
-#Quantilfying all variables with mpg
+#Quantifying all variables with mpg
 fit<-lm(mpg~. -1,data=mtcars)
 summary(fit) $coef
 
@@ -21,11 +21,15 @@ summary(fit) $coef
 lm.fit<-lm(mpg~as.factor(am), data=mtcars)
 summary(lm.fit)$coef
 
-#Qunatifying Estimate of 7.245 with Generalized Linear Model
+#Quantifying Estimate of 7.245 with Generalized Linear Model
 glm.fit<-glm(mpg~as.factor(am), data=mtcars)
 
 #Residual Plot
-
+am.fit<-lm(mpg~am, data=mtcars)
+plot(resid(am.fit)~mtcars$am,pch=19,col="lightblue",
+     xlab = "Transmission Type (0 for Automatic, 1 for Manual)",
+     ylab = "Residuals")
+abline(h=0, col="red", lwd=3)
 
 #Uncertainty in conclusions
 
